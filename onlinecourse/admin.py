54a6131293086cac746fc422735d7f43785e9f42
Nguyen Dinh Thuy <ndthuy21092005@gmail.com>
2026-06-01 11:54:00 +0700

    Create admin.py

---
+++ onlinecourse/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Course, Lesson, Question, Choice, Submission
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+admin.site.register(Course)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
